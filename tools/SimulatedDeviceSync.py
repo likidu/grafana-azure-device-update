7d67c0499124e973814b1fe65e3c3a294f8635f0
Liya Du <liydu@microsoft.com>
2023-02-13 12:09:54 -0800

    Add IoT Hub testing script

---
+++ tools/SimulatedDeviceSync.py
@@ -0,0 +1,114 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import os
+import random
+import time
+
+import click
+from azure.iot.device import IoTHubDeviceClient, Message, MethodResponse
+
+# The device connection string to authenticate the device with your IoT hub.
+# Using the Azure CLI:
+# az iot hub device-identity connection-string show --device-id MyDevKit --hub-name {YourIoTHubName} --output table
+# CONNECTION_STRING = os.getenv("IOTHUB_DEVICE_CONNECTION_STRING")
+
+# CONNECTION_STRING = 'HostName=pomelo-iot.azure-devices.net;DeviceId=TestDevice;SharedAccessKey=PQ34b7qLnxX6fh6QLtsHL4dAfXTlFSP3EXuyKZlhSUw='
+
+# CONNECTION_STRING = 'HostName=pomelo-iot.azure-devices.net;DeviceId=AnotherTestDevice;SharedAccessKey=l/WIJ5l2FuL10HHGphgLPyDMW+DWyVQWKk79bkRSD3U='
+
+# Define the JSON message to send to IoT Hub.
+TEMPERATURE = 20.0
+HUMIDITY = 60
+MSG_TXT = '{{"temperature": {temperature},"humidity": {humidity}}}'
+
+INTERVAL = 3
+
+
+def create_client(device):
+    # Create an IoT Hub client
+    client = IoTHubDeviceClient.create_from_connection_string(device)
+
+    # Define a method request handler
+    def method_request_handler(method_request):
+        if method_request.name == "SetTelemetryInterval":
+            try:
+                global INTERVAL
+                INTERVAL = int(method_request.payload)
+            except ValueError:
+                response_payload = {"Response": "Invalid parameter"}
+                response_status = 400
+            else:
+                response_payload = {
+                    "Response": "Executed direct method {}".format(method_request.name)}
+                response_status = 200
+        else:
+            response_payload = {
+                "Response": "Direct method {} not defined".format(method_request.name)}
+            response_status = 404
+
+        method_response = MethodResponse.create_from_method_request(
+            method_request, response_status, response_payload)
+        client.send_method_response(method_response)
+
+    try:
+        # Attach the method request handler
+        client.on_method_request_received = method_request_handler
+    except:
+        # Clean up in the event of failure
+        client.shutdown()
+        raise
+
+    return client
+
+
+def run_telemetry_sample(client):
+    # This sample will send temperature telemetry every second
+    print("IoT Hub device sending periodic messages")
+
+    client.connect()
+
+    while True:
+        # Build the message with simulated telemetry values.
+        temperature = TEMPERATURE + (random.random() * 15)
+        humidity = HUMIDITY + (random.random() * 20)
+        msg_txt_formatted = MSG_TXT.format(
+            temperature=temperature, humidity=humidity)
+        message = Message(msg_txt_formatted)
+
+        # Add a custom application property to the message.
+        # An IoT hub can filter on these properties without access to the message body.
+        if temperature > 30:
+            message.custom_properties["temperatureAlert"] = "true"
+        else:
+            message.custom_properties["temperatureAlert"] = "false"
+
+        # Send the message.
+        print("Sending message: {}".format(message))
+        client.send_message(message)
+        print("Message sent")
+        time.sleep(INTERVAL)
+
+
+@click.command()
+@click.option("--device", help="You IoT Hub Device Connection String.")
+def main(device):
+    print("IoT Hub Quickstart #2 - Simulated device")
+    print("Press Ctrl-C to exit")
+
+    # Instantiate the client. Use the same instance of the client for the duration of
+    # your application
+    client = create_client(device)
+
+    # Send telemetry
+    try:
+        run_telemetry_sample(client)
+    except KeyboardInterrupt:
+        print("IoTHubClient sample stopped by user")
+    finally:
+        print("Shutting down IoTHubClient")
+        client.shutdown()
+
+
+if __name__ == '__main__':
+    main()
